1fc3d93c5e860860b560de70318ad39af00bf6e4
Gecnos <vianneyhoueho@gmail.com>
2025-07-20 22:12:40 +0100

    Mise en place du système de routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NoteCard} from './note-card/note-card';
+import { NoteForm } from './note-form/note-form';
+import { NoteList } from './note-list/note-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NoteCard, NoteForm, NoteList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
